9fb54e69921bde91a3ad2c787c15d4b573d317c8
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-30 10:18:44 -0400

    import and map sidebarLinks in LeftSidebar comp

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,8 +1,12 @@
+import { sidebarLinks } from '@/constants';
+
 function LeftSidebar() {
   return (
     <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
-        
+        {sidebarLinks.map((link) => (
+          <div>Link</div>
+        ))}
       </div>
     </section>
   )
