8cd232ae26db28f45b6742b288ebaefea3ca5a4c
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-30 10:42:28 -0400

    add className to main container in layout and setup initial rightSidebar comp

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,5 +1,11 @@
 function RightSidebar() {
-  return <h1>RightSidebar</h1>
+  return (
+    <section className="custom-scrollbar rightsidebar">
+      <div className="flex flex-1 flex-col justify-start">
+        <h3>Suggested Communities</h3>
+      </div>
+    </section>
+  )
 }
 
 export default RightSidebar;
