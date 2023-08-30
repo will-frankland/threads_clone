1531af11befca8c4a8f51c4675f634e9b2f1dc5e
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-30 11:34:36 -0400

    install shadui form

---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import useForm from 'react-hook-form';
+import {
+  Form
+} from '@components/ui/form';
+
+interface Props {
+  user: {
+    id: string;
+    objectId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+  },
+  btnTitle: string;
+}
+
+const AccountProfile = ({ user, btnTitle }: Props) => {
+  const form = useForm();
+  return (
+    <Form>
+      
+    </Form>
+  )
+}
+
+export default AccountProfile;
