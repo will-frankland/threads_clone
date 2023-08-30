9ca1013a4a78497d333d79cba1af3707dc9434c5
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-30 10:32:11 -0400

    copy signout into leftbar comp, style and add callback to redirect to signin

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -5,6 +5,9 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { usePathname, useRouter } from 'next/navigation';
 
+//Clerk Imports
+import { SignOutButton, SignedIn } from "@clerk/nextjs";
+
 // Constant Imports
 import { sidebarLinks } from '@/constants';
 
@@ -31,6 +34,21 @@ function LeftSidebar() {
             <p className='text-light-1 max-lg:hidden'>{link.label}</p>
           </Link>
         )})}
+      </div>
+      <div className='mt-10 px-6'>
+      <SignedIn>
+          <SignOutButton signOutCallback={() => router.push('/sign-in')}>
+            <div className="flex cursor-pointer gap-4 p-4">
+              <Image
+                src="/assets/logout.svg"
+                alt="logout"
+                width={24}
+                height={24}
+              />
+              <p className='text-light-2 max-lg:hidden'>Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
       </div>
     </section>
   )
