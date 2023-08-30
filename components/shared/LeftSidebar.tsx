372ce4e30caf9e04749acc4ea9cd613ddfc07101
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-30 10:29:08 -0400

    import useRouter and usePathname and setup isActive constant to style classname of selected sidebar comp

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,19 +1,26 @@
+"use client"
+
 // Next Imports
 import Link from 'next/link';
 import Image from 'next/image';
+import { usePathname, useRouter } from 'next/navigation';
 
 // Constant Imports
 import { sidebarLinks } from '@/constants';
 
 function LeftSidebar() {
+  const router = useRouter();
+  const pathname = usePathname();
   return (
     <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
-        {sidebarLinks.map((link) => (
+        {sidebarLinks.map((link) => {
+          const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
+        return (
           <Link
             href={link.route}
             key={link.label}
-            className='leftsidebar_link'
+            className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}
           >
             <Image
               src={link.imgURL}
@@ -23,7 +30,7 @@ function LeftSidebar() {
             />
             <p className='text-light-1 max-lg:hidden'>{link.label}</p>
           </Link>
-        ))}
+        )})}
       </div>
     </section>
   )
