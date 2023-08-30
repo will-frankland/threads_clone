61b0dd4ed83b63cc67d321a90dc674d335e3b1a5
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-30 09:35:10 -0400

    setup ClerkProvider and signin/signup with layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,27 +1,24 @@
-import { ClerkProvider } from '@clerk/nextjs';
-import { Inter } from 'next/font/google';
-import '../globals.css'
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import "../globals.css";
 
 export const metadata = {
-  title: 'Threads Clone',
-  description: ' A Next.js 13 Meta Threads Clone App'
-}
+  title: "Threads Clone",
+  description: " A Next.js 13 Meta Threads Clone App",
+};
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
-  children
+  children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-  <ClerkProvider>
-    <html lang="en">
-      <body className={`${inter.className} bg-dark-1`}>
-        {children}
-      </body>
-
-    </html>
-  </ClerkProvider>
-  )
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
 }
