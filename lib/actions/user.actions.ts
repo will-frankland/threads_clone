ab089084accbc6237f97964a0953a194a4d6c766
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-31 10:47:35 -0400

    create user action to update user

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,8 +1,37 @@
-"use server"
+"use server";
+import { connectToDb } from "../mongoose";
 
-import { connectToDb } from "../mongoose"
+// Model Imports
+import User from "../models/user.model";
+import { revalidatePath } from "next/cache";
 
-export async function updateUser(): Promise<void> {
+export async function updateUser(
+  userId: string,
+  username: string,
+  name: string,
+  bio: string,
+  image: string,
+  path: string
+): Promise<void> {
   connectToDb();
 
+  try {
+    await User.findOneAndUpdate(
+      { id: userId },
+      {
+        username: username.toLowerCase(),
+        name,
+        bio,
+        image,
+        onboarded: true,
+      },
+      { upsert: true }
+    );
+
+    if (path === "/profile/edit") {
+      revalidatePath(path); // allows you to revalidate data associated with a specific path
+    }
+  } catch (error: any) {
+    throw new Error(`Failed to create/update user: ${error.message}`);
+  }
 }
