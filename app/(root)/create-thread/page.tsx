89a018d761444b8ffff57b2c73d4339e9fe2a23d
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-31 13:21:17 -0400

    create PostThread component and pass into create-thread comp

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -1,12 +1,23 @@
 import { currentUser } from "@clerk/nextjs";
-import redirect from "next/navigation";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
+import PostThread from "@/components/forms/PostThread";
 
 async function Page() {
   const user = await currentUser();
 
   if(!user) return null;
-  
-  return <h1 className="head-text">Create Thread</h1>
+
+  const userInfo = await fetchUser(user.id);
+
+  if(!userInfo?.onboarded) redirect('/onboarding');
+
+  return (
+    <>
+      <h1 className="head-text">Create Thread</h1>
+      <PostThread userId={userInfo._id}/>
+    </>
+  )
 }
 
 export default Page;
