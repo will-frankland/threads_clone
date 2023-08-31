a3b5623dd0f95162c9bde75676d7791e1684d6d6
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-31 15:38:17 -0400

    create thread model and create thread actions files

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -50,15 +50,15 @@ function PostThread({ userId }: { userId: string }) {
     },
   });
 
-  const onSubmit = () => {
-    
-  }
+  const onSubmit = async () => {
+    // await createThread()
+  };
 
   return (
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="flex flex-col justify-start gap-10"
+        className="mt-10 flex flex-col justify-start gap-10"
       >
         <FormField
           control={form.control}
@@ -69,15 +69,15 @@ function PostThread({ userId }: { userId: string }) {
                 Content
               </FormLabel>
               <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
-                <Textarea
-                  rows={15}
-                  {...field}
-                />
+                <Textarea rows={15} {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
+        <Button type="submit" className="bg-primary-500">
+          Post Thread
+        </Button>
       </form>
     </Form>
   );
---
+++ lib/actions/thread.actions.ts
@@ -0,0 +1,33 @@
+import { connectToDb } from "../mongoose";
+import Thread from "../models/thread.model";
+import User from "../models/user.model";
+import { revalidatePath } from "next/cache";
+
+interface Params {
+  text: string;
+  author: string;
+  communityId: string | null;
+  path: string;
+}
+
+export async function createThread({
+  text,
+  author,
+  communityId,
+  path,
+}: Params) {
+  connectToDb();
+
+  const createdThread = await Thread.create({
+    text,
+    author,
+    community: null,
+  });
+
+  // Update user model
+  await User.findByIdAndUpdate(author, {
+    $push: { threads: createdThread._id }
+  })
+
+  revalidatePath(path);
+}
