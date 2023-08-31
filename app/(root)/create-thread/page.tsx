9797cba5c164fb0e26ac9899ec39b1d3fde53d4f
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-31 11:24:45 -0400

    add currentUser imports and setup code to check current user/redirect if not logged in

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -1,4 +1,11 @@
+import { currentUser } from "@clerk/nextjs";
+import redirect from "next/navigation";
+
 async function Page() {
+  const user = await currentUser();
+
+  if(!user) return null;
+  
   return <h1 className="head-text">Create Thread</h1>
 }
 
