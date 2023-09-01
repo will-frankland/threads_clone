cdc4db6c88223c602c9713377d12ae825402d854
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-09-01 12:55:44 -0400

    implement isComment and render only when comments are available

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -34,6 +34,7 @@ const ThreadCard = ({
   community,
   createdAt,
   comments,
+  isComment,
 }: Props) => {
   return (
     <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
@@ -66,7 +67,7 @@ const ThreadCard = ({
                   height={24}
                   className="cursor-pointer object-contain"
                 />
-                <Link>
+                <Link href={`/thread/${id}`}>
                   <Image
                     src="/assets/reply.svg"
                     alt="reply"
@@ -90,6 +91,11 @@ const ThreadCard = ({
                   className="cursor-pointer object-contain"
                 />
               </div>
+              {isComment && comments.length > 0 && (
+                <Link href={`/thread/${id}`}>
+                  <p className="mt-1 text-semibold-medium text-gray-1">{comments.length} replies</p>
+                </Link>
+              )}
             </div>
           </div>
         </div>
