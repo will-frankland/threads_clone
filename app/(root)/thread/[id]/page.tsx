cb4db0a81c0c957f8eec8b5c94b95f4050bcfdbc
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-09-01 14:05:35 -0400

    create Comment component and define interface with initial setup. Pass props and render inside page with props passed

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -3,6 +3,7 @@ import { currentUser } from "@clerk/nextjs";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
 import { fetchThreadById } from "@/lib/actions/thread.actions";
+import Comment from "@/components/forms/Comment";
 
 const Page = async ({ params }: { params: { id: string } }) => {
   if(!params.id) return null
@@ -28,6 +29,13 @@ const Page = async ({ params }: { params: { id: string } }) => {
         community={thread.community}
         createdAt={thread.createdAt}
         comments={thread.children}
+      />
+    </div>
+    <div className="mt-7">
+      <Comment
+        threadId={thread.id}
+        currentUserImg={user.imageUrl}
+        currentUserId={JSON.stringify(userInfo._id)}
       />
     </div>
   </section>
---
+++ components/forms/Comment.tsx
@@ -0,0 +1,15 @@
+interface Props {
+  threadId: string;
+  currentUserImg: string;
+  currentUserId: string;
+}
+
+const Comment = ({ threadId, currentUserImg, currentUserId }: Props) => {
+  return (
+  <div>
+    <h1 className="text-white">Comment Form</h1>
+  </div>
+  )
+}
+
+export default Comment;
