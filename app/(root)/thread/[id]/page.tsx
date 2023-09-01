40ca3737ab169cd9c1688b30adccccee98c7133a
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-09-01 13:13:12 -0400

    import fetchThreadById to page and pass data down to ThreadCard comp

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -2,6 +2,7 @@ import ThreadCard from "@/components/cards/ThreadCard";
 import { currentUser } from "@clerk/nextjs";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
+import { fetchThreadById } from "@/lib/actions/thread.actions";
 
 const Page = async ({ params }: { params: { id: string } }) => {
   if(!params.id) return null
