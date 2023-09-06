5f568763bfd702f362ed0e7f0d4ba8902e0d3259
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-09-06 11:05:12 -0400

    create ProfileHeader comp, add props and pass via Profile page

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,7 +1,28 @@
-const Page = async () => {
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import ProfileHeader from "@/components/shared/ProfileHeader";
+
+async function Page({ params }: { params: { id: string} }) {
+  const user = await currentUser();
+
+  if(!user) return null;
+
+  const userInfo = await fetchUser(params.id);
+
+  if(!userInfo?.onboarded) redirect('/onboarding');
+
   return (
     <section>
-      Profile
+      <ProfileHeader
+        accountId={userInfo.id}
+        authUserId={user.id}
+        name={userInfo.name}
+        username={userInfo.username}
+        imgUrl={userInfo.imgUrl}
+        bio={userInfo.bio}
+      />
     </section>
   )
 }
